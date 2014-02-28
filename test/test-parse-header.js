6cdb81bd29a5fc9f268ae0f067ef85344b85a76f
Brian White <mscdex@mscdex.net>
2014-02-27 19:11:52 -0500

    Parser: support optional language in mime encoded words (RFC2231)

--- test/test-parse-header.js
+++ test/test-parse-header.js
@@ -20,6 +20,14 @@ var CRLF = '\r\n';
     expected: { subject: [ '¡Hola, señor!' ] },
     what: 'MIME encoded-word in value'
   },
+  { source: ['Subject: =?iso-8859-1*es?Q?=A1Hola,_se=F1or!?=', CRLF],
+    expected: { subject: [ '¡Hola, señor!' ] },
+    what: 'MIME encoded-word in value with language set (RFC2231)'
+  },
+  { source: ['Subject: =?iso-8859-1*?Q?=A1Hola,_se=F1or!?=', CRLF],
+    expected: { subject: [ '¡Hola, señor!' ] },
+    what: 'MIME encoded-word in value with empty language set'
+  },
   { source: ['Subject: =?GB2312?Q?=B2=E2=CA=D4=CC=E2=C4=BF=D3=EB=D6=D0=B9=FA=D0=C5_long_subjects_are_not_OK_12?=', CRLF,
              ' =?GB2312?Q?345678901234567890123456789012345678901234567890123456789012?=', CRLF,
              ' =?GB2312?Q?345678901234567890?=', CRLF],
